7097ff663cf6841408579be3b1b5f797d4f0481f
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-12-25 10:00:30 -0300

    fix: back to basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,9 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
-import { Home } from "./routes/home"
-import { Layout } from "./components/layout"
-import { CarbonButton } from "./routes/components/button/carbon"
-import { ButtonPage } from "./routes/components/button"
-import { AlertPage } from "./routes/components/alert"
-import { DialogPage } from "./routes/components/dialog"
-import { SelectPage } from "./routes/components/select"
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { path: '/', element: <Home /> },
-      { 
-        path: 'components', 
-        children: [
-          { index: true, element: <Navigate to="/components/button" replace /> },
-          {
-            path: 'button',
-            element: <ButtonPage />,
-            children: [
-              {
-                path: 'carbon',
-                element: <CarbonButton />
-              }
-            ]
-          },
-          {
-            path: 'alert',
-            element: <AlertPage />,
-          },
-          {
-            path: 'dialog',
-            element: <DialogPage />,
-          },
-          {
-            path: 'select',
-            element: <SelectPage />,
-          }
-        ]
-      },
-      // { path: '/pages', element: <Pages /> },
-    ]
-  }
-])
-
 export function App() {
   return (
-   <RouterProvider router={router} />
+    <main className="w-fulll h-screen bg-zinc-100 flex justify-center items-center">
+      <div>
+        <button>Hello!</button>
+      </div>
+    </main>
   )
 }
--- src/routes/components/button/carbon.tsx
+++
@@ -1,5 +0,0 @@
-export function CarbonButton() {
-  return (
-    <button>Click me!</button>
-  )
-}
